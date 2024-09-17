d698e2fa40a8c408c4a240968889a3be0da12dbc
PonVignesh <98687019+PonVicky@users.noreply.github.com>
2024-09-17 20:16:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Good String/good-string.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0){
+            String s = read.readLine().trim();
+            Solution ob = new Solution();
+            String ans = ob.isGoodString(s);
+            System.out.println(ans);
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    String isGoodString(String s) 
+    { 
+        // code here
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+} 
